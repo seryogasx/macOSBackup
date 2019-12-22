c301bf148fd14a1a72b6dda244db9fafb8f6c0d6
e1nfalt <sergio.petrovx@yandex.ru>
2019-12-22 20:35:04 +0300

    beta without rotation

--- macOSBackup/main.swift
+++ macOSBackup/main.swift
@@ -1,11 +1,101 @@
 import Foundation
-//import CommonCrypto
 import CryptoKit
+import SQLite3
 
 
 extension String {
     var sha256: String {
         String(SHA256.hash(data: Data(self.utf8)).description.suffix(64))
+    }
+}
+
+
+// MARK: Item
+struct Item {
+    let modificationDate: Date
+    let fullPath: String
+    let name: String
+}
+
+
+// MARK: BackuperError
+enum BackuperError: Error {
+    case itemNotExists(path: String)
+    case itemAlreadyExists(path: String)
+    case emptyBackup
+}
+
+
+// MARK: BackupState
+struct BackupInfo {
+    var hashName: String
+    var FullBackupDate: Date
+}
+
+
+// MARK: Database
+class Database {
+    
+    let dbUrl : URL
+    var db : OpaquePointer?
+    var statement : OpaquePointer?
+    public static var shared = Database()
+    
+    private init() {
+        dbUrl = try! FileManager.default.url(
+            for: .documentDirectory,
+            in: .userDomainMask,
+            appropriateFor: nil,
+            create: false).appendingPathComponent("macOSBackupDatabase.sqlite")
+
+        if sqlite3_open(dbUrl.path, &db) != SQLITE_OK {
+            print("Error opening database")
+        }
+    }
+    
+    func tableInit() {
+        let query = "CREATE TABLE IF NOT EXISTS BackupInfo (BackupName varchar(64) primary key, ModificationDate integer); "
+        
+        if sqlite3_exec(db, query, nil, nil, nil) != SQLITE_OK {
+            print("Error creating Tables!")
+        }
+        sqlite3_finalize(statement)
+    }
+    
+    func addBackupInfo(info: BackupInfo) {
+        tableInit();
+        var query = "SELECT * FROM BackupInfo WHERE BackupName=?"
+        var lastFullBackupDate: Int32?
+        if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+            sqlite3_bind_text(statement, 1, info.hashName, -1, nil)
+            
+            if sqlite3_step(statement) == SQLITE_ROW {
+                lastFullBackupDate = sqlite3_column_int(statement, 1)
+            }
+        }
+        
+        if let _ = lastFullBackupDate {
+            query = "UPDATE BackupInfo SET ModificationDate=? WHERE BackupName=?"
+            if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+                sqlite3_bind_int(statement, 1, Int32(info.FullBackupDate.timeIntervalSince1970))
+                sqlite3_bind_text(statement, 2, info.hashName, -1, nil)
+                
+                if sqlite3_step(statement) != SQLITE_DONE {
+                    print("Error update database!")
+                }
+            }
+        }
+        else {
+            query = "INSERT INTO BackupInfo (BackupName, ModificationDate) VALUES (?, ?)"
+            if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK {
+                sqlite3_bind_text(statement, 1, info.hashName, -1, nil)
+                sqlite3_bind_int(statement, 2, Int32(info.FullBackupDate.timeIntervalSince1970))
+                
+                if sqlite3_step(statement) != SQLITE_DONE {
+                    print("Error add to database!")
+                }
+            }
+        }
     }
 }
 
@@ -25,21 +115,8 @@ class Backuper {
     private let dedDestinationPath: String
     private let fm = FileManager.default
     private let sourceIdentifier: String
+    private var db = Database.shared
     
-    // MARK: Item
-    struct Item {
-        let modificationDate: Date
-        let fullPath: String
-        let name: String
-    }
-
-
-    // MARK: BackuperError
-    enum BackuperError: Error {
-        case itemNotExists(path: String)
-        case itemAlreadyExists(path: String)
-        case emptyBackup
-    }
     
     init(args: [String]) {
         backupSourceDir = fm.currentDirectoryPath
@@ -96,7 +173,10 @@ class Backuper {
     }
     
     private func firstBackup() throws {
-        
+        db.addBackupInfo(info: BackupInfo(hashName: sourceIdentifier, FullBackupDate: Date()))
+        for i in backupItemsList {
+            try fm.copyItem(atPath: i.fullPath, toPath: dedDestinationPath + "/" + i.name)
+        }
     }
     
     private func rotate() throws {
@@ -130,6 +210,7 @@ class Backuper {
         return "Failed! \(error ?? "Unknown")"
     }
 }
+
 
 let mainFunc = Backuper(args: CommandLine.arguments)
 print(mainFunc.run())
