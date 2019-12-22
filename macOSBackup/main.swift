9f98b69e7dcc1943b6588c2ea180b8b730746dbc
e1nfalt <sergio.petrovx@yandex.ru>
2019-12-22 12:59:00 +0300

    Initial Commit

---
+++ macOSBackup/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macOSBackup
+//
+//  Created by Сергей Петров on 22.12.2019.
+//  Copyright © 2019 SergioPetrovx. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
