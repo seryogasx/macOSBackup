bace854987c9322d609b716a669fa79dec84cd7c
e1nfalt <sergio.petrovx@yandex.ru>
2019-12-26 14:33:00 +0300

    releaze version

---
+++ macOSBackup/database.swift
@@ -0,0 +1,9 @@
+//
+//  database.swift
+//  macOSBackup
+//
+//  Created by Сергей Петров on 26.12.2019.
+//  Copyright © 2019 SergioPetrovx. All rights reserved.
+//
+
+import Foundation
---
+++ macOSBackup/obj.swift
@@ -0,0 +1,9 @@
+//
+//  obj.swift
+//  macOSBackup
+//
+//  Created by Сергей Петров on 26.12.2019.
+//  Copyright © 2019 SergioPetrovx. All rights reserved.
+//
+
+import Foundation
